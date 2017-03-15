7a18ca755546ca3acc2a3346073699a26c866466
MahimaSusan <mahima@qburst.com>
2017-03-15 16:53:33 +0530

    config change

--- webpack.config.js
+++ webpack.config.js
@@ -4,34 +4,16 @@ module.exports = {
     path: __dirname,
     filename: "bundle.js"
   },
-    module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [{
-                loader: "style-loader" // creates style nodes from JS strings
-            }, {
-                loader: "css-loader" // translates CSS into CommonJS
-            }, {
-                loader: "sass-loader" // compiles Sass to CSS
-            }]
-        }]
-    }
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [{
+        loader: "style-loader" // creates style nodes from JS strings
+      }, {
+        loader: "css-loader" // translates CSS into CommonJS
+      }, {
+        loader: "sass-loader" // compiles Sass to CSS
+      }]
+    }]
+  }
 };
-
-
-// webpack.config.js
-// module.exports = {
-//     ...
-//     module: {
-//         rules: [{
-//             test: /\.scss$/,
-//             use: [{
-//                 loader: "style-loader" // creates style nodes from JS strings
-//             }, {
-//                 loader: "css-loader" // translates CSS into CommonJS
-//             }, {
-//                 loader: "sass-loader" // compiles Sass to CSS
-//             }]
-//         }]
-//     }
-// };
