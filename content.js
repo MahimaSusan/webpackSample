2f6c74e9282ffeabeb87c9023820f4b6d51c49f4
MahimaSusan <mahima@qburst.com>
2017-03-17 12:05:08 +0530

    image fetch

--- content.js
+++ content.js
@@ -3,20 +3,8 @@ $("a").click(function() {
   $(".picture-section").css("display", "block");
 });
 window.onload = function() {
-          	 $("<span>Hello world!</span>").insertAfter(".gallery-section");
-          }
-
-// var myImage = document.querySelector('img');
-
-// fetch('./images/gallery-img-1.jpg')
-// .then(function(response) {
-//   return response.blob();
-// })
-// .then(function(myBlob) {
-//   var objectURL = URL.createObjectURL(myBlob);
-//     console.log("frjr");
-//   myImage.src = objectURL;
-// });
+  $("<span>Hello world!</span>").insertAfter(".gallery-section");
+}
 fetch('images.json')
   .then(
     function(response) {
@@ -25,30 +13,25 @@ fetch('images.json')
           response.status);
         return;
       }
-
-      // Examine the text in the response  
       response.json().then(function(data) {
-        console.log(data);
-        data.images.forEach(function(obj) {
-          var img = new Image();
-          img.src = obj.bannerImg1;
-          img.setAttribute("class", "banner-img");
-          img.setAttribute("alt", "effy");
-          document.getElementById("img-container").appendChild(img);
-          $("#img-container").append("<div></div>")
-
+        $.each(data.gallerySection, function(index, item) {
+          $.each($(".picture-gallery-first"), function(def, dup) {
+            if (index == def) {
+              $(dup).attr("srcset", item.src);
+            }
+          });
         });
 
+        $.each(data.pictureSection, function(index, item) {
+          $.each($(".image-bottom"), function(def, dup) {
+            if (index == def) {
+              $(dup).attr("srcset", item.src);
+            }
+          });
+        });
       });
     }
   )
   .catch(function(err) {
     console.log('Fetch Error :-S', err);
   });
-//   data.images.forEach( function(obj) {
-//   var img = new Image();
-//   img.src = obj.bannerImg1;
-//   img.setAttribute("class", "banner-img");
-//   img.setAttribute("alt", "effy");
-//   document.getElementById("img-container").appendChild(img);
-// });
